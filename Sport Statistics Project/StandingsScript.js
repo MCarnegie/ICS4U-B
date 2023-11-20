32fb1a6fae1a156372936c9f986e971bed6eebdc
MCarnegie <mcarnegie@bayviewglen.ca>
2023-11-20 10:23:00 -0500

    made stuff into seperate pages so i can make it nicer later

--- Sport Statistics Project/StandingsScript.js
+++ Sport Statistics Project/StandingsScript.js
@@ -1,59 +1,6 @@
-// Get the form and list elements
-const form = document.getElementById('scoreForm');
-const list = document.getElementById('scoreList');
-
-// Check if local storage has existing scores
-let scoreData = JSON.parse(localStorage.getItem('scoreData')) || [];
-
-// Function to update the list and save to local storage
-function updateList() {
-    list.innerHTML = '';
-    scoreData.forEach((score, index) => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `<span>${score.team1}: ${score.team1Score} - ${score.team2}: ${score.team2Score} -</span> <a href="#" onclick="removeScore(${index})">Remove</a>`;
-        list.appendChild(listItem);
-    });
-    localStorage.setItem('scoreData', JSON.stringify(scoreData));
-}
-
-// Function to add game scores from the form
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const team1Select = document.getElementById('team1');
-    const team1ScoreInput = document.getElementById('team1Score');
-    const team2Select = document.getElementById('team2');
-    const team2ScoreInput = document.getElementById('team2Score');
-
-    const team1 = team1Select.value;
-    const team1Score = parseInt(team1ScoreInput.value);
-    const team2 = team2Select.value;
-    const team2Score = parseInt(team2ScoreInput.value);
-
-    if (!isNaN(team1Score) && !isNaN(team2Score)) {
-        const score = { team1, team1Score, team2, team2Score };
-        scoreData.push(score);
-        team1Select.value = 'Team A';
-        team1ScoreInput.value = ''; // Clear the input field for Team 1
-        team2Select.value = 'Team A';
-        team2ScoreInput.value = ''; // Clear the input field for Team 2
-        updateList();
-        updateTable();
-    }
-});
-
-// Function to remove a score from the list
-function removeScore(index) {
-    scoreData.splice(index, 1);
-    updateList();
-    updateTable();
-}
-
-// Initial list population
-updateList();
-
 //populating the table
 const tbody = document.querySelector('tbody')
-
+let scoreData = JSON.parse(localStorage.getItem('scoreData')) || [];
 
 function updateTable(){
     let allStats = getAllStats();
