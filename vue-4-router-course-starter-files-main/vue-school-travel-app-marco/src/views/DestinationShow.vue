f89b6e4bcb23234a7d080fe71fd1204d4ed12144
MCarnegie <mcarnegie@bayviewglen.ca>
2024-01-26 17:06:18 -0500

    forgot to commit most of it

---
+++ vue-4-router-course-starter-files-main/vue-school-travel-app-marco/src/views/DestinationShow.vue
@@ -0,0 +1,34 @@
+
+
+
+<template>
+    <section class="destination">
+        <h1>{{ destination.name }}</h1>
+
+        <div class="destination-details">
+            <img :src="`/images/${destination.image}`" :alt="ajsdh">
+            <p>{{ destination.description }}</p>
+        </div>
+    </section>
+        
+  
+</template>
+
+<script>
+
+import sourceData from "@/data.json"
+
+
+export default{
+    computed:{
+        destinationId(){
+            //anything coming from route is a string
+            return parseInt(this.$route.params.id);
+        },
+        destination(){
+            return sourceData.destinations.find(destination => destination.id === this.destinationId)
+        }
+    }
+    
+}
+</script>
