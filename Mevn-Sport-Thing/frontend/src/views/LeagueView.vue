04fbd3c6bb88ebd82dc841d37a5cff3bbf63ca6f
MCarnegie <mcarnegie@bayviewglen.ca>
2024-05-02 12:28:28 -0400

    trying

--- Mevn-Sport-Thing/frontend/src/views/LeagueView.vue
+++ Mevn-Sport-Thing/frontend/src/views/LeagueView.vue
@@ -14,7 +14,7 @@ export default {
     const link = `${API_URL}/api/league`;
     onMounted(async () => {
       try {
-        const response = await axios.get(`${API_URL}/api/league`);
+        const response = await axios.get(`${API_URL}api/league`);
         leagues.value = response.data; 
       } catch (error) {
         console.error('Error fetching data:', error);
