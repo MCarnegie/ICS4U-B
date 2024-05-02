aecf0790182b2d0f76b13dc85715e0bfefb23caf
MCarnegie <mcarnegie@bayviewglen.ca>
2024-05-02 14:27:09 -0400

    asdas

--- Mevn-Sport-Thing/server/index.js
+++ Mevn-Sport-Thing/server/index.js
@@ -25,7 +25,7 @@ require('./db');
 const app = express();
 const PORT  = process.env.PORT || 3000;
 const corsOptions = {
-    origin: 'http://localhost:5173/',
+    origin: 'https://frontend-mevn.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // allow cookies to be sent cross-origin
